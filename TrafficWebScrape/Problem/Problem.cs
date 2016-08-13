036c29f5410aac111a2c1e151419dbe289ee477f
Fraham <frahamp@gmail.com>
2016-08-14 00:56:25 +0100

    New class problem

---
+++ TrafficWebScrape/Problem/Problem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficWebScrape.Problem
+{
+    class Problem
+    {
+        private string reason;
+
+        public Problem(string reason)
+        {
+            Reason = reason;
+        }
+
+        public string Reason
+        {
+            get
+            {
+                return reason;
+            }
+
+            set
+            {
+                if (value == null || value.Equals(""))
+                {
+                    value = "Unknown";
+                }
+
+                reason = value;
+            }
+        }
+    }
+}
