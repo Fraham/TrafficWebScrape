7faf0cdcaaa9b349fd36afe740f01e2849c42174
Fraham <frahamp@gmail.com>
2016-08-16 14:01:55 +0100

    New problem tests

---
+++ TrafficWebScrapeTest/Problem/TrafficProblemTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrafficWebScrapeTest.Problem
+{
+    [TestClass]
+    public class TrafficProblemTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
